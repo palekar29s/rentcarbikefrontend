8e4f378af74f9038069f707d7a34de570b83d4c4
Shruti Palekar <palekarshruti1@gmail.com>
2025-12-04 11:15:18 +0530

    Update home.component.ts

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,6 +1,7 @@
 import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { HttpClient, HttpParams } from '@angular/common/http';
 
 interface Vehicle {
   type: string;
